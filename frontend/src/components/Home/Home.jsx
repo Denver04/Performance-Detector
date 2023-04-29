c2915aa5f68df8fb162e4254398d2eabb49623b9
Abhisek kumar singh <87076425+Denver04@users.noreply.github.com>
2023-04-29 17:43:55 +0530

    updated name

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ import "./home.css"
 function Home() {
   return (
     <div className="home">
-        <h1>AI-Mini Project</h1>
+        <h1>Performance Evaluation</h1>
         <p>Get the Employee performance based on how he/she works.</p>
         <p>Fill the required form and get the performance.</p>
     </div>
